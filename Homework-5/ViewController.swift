5fd51b644ba1de9667eced5fd9c59814789410ec
Andrey Neverovskiy <never111194@gmail.com>
2024-04-11 17:39:20 +0300

    XX-3. print added

--- Homework-5/ViewController.swift
+++ Homework-5/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         createView()
-        
+        print("Yes")
     }
     
     //MARK: - Flow Functions
